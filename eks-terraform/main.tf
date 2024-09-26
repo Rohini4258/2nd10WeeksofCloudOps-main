2683a932581b99445309ab14dd4ea00a98b16f0b
Rohini4258 <rohini4258@gmail.com>
2024-09-26 11:00:38 -0400

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -169,7 +169,7 @@ data "aws_security_group" "selected" {
     instance_types  = ["t3.large"]
 
     remote_access {
-      ec2_ssh_key               = "DevopsProject"
+      ec2_ssh_key               = "DevOpsProject"
       source_security_group_ids = [data.aws_security_group.selected.id]
     }
 
